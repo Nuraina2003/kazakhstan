7350469997a076b1d2f2c90700be8c1cf501cac2
Nuraina2003 <200103126stu.sdu.edu.kz>
2022-04-29 11:55:43 +0600

    first commit

---
+++ labka/forms.py
@@ -0,0 +1,34 @@
+from django import forms
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+
+User = get_user_model()
+
+from .models import Registration
+
+
+class AddPostForm(forms.ModelForm):
+    # def __init__(self,*args, **kwargs):
+    #     super().__init__(*args, **kwargs)
+    #     self.fields['category'].empty_label = "Таңдаңыз"
+
+    password = forms.CharField(widget=forms.PasswordInput)
+    class Meta:
+        model = Registration
+        fields = '__all__'
+
+class LoginUserForm(AuthenticationForm):
+        username = forms.CharField(label='Login', widget=forms.TextInput(attrs={'class': 'form-input'}))
+        password = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
+
+class UserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = User
+
+class EmailForm(forms.Form):
+    email = forms.EmailField(label='Пайдаланушы аты')
+    subject = forms.CharField(max_length=100)
+    attach = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True}))
+    message = forms.CharField(widget=forms.Textarea)
